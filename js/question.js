3bfccc7e0efa44f2229b360ed9106eb623020a3a
GODofMEGANE <s18195@tokyo.kosen-ac.jp>
2021-12-11 21:27:15 +0900

    [add] 基本的な部分が完成

--- js/question.js
+++ js/question.js
@@ -1,4 +1,14 @@
 'use strict';
+const ANSWER_HASH = new Array(
+    "c884a2af4f0ab6fe08b2d7158ec3359fd880c779f238728914dd2a72823a65b2",
+    "e9de08263f4ad49131d3dd1dc28694d26baa1e9c3033f39a4885d7690b06d5b3",
+    "4a41357c7d510b4fc7cee057b0e9452a3548217c91264dca9200e6de5e77721f",
+    "e81dfe69841ad2f7b5790b63e998f0febaf3b29acd732881975130761b98e2c7",
+    "599f8341fe3e0fd23de7d89500b8224965ab536307049a175bcd30331d0918c5",
+    "5944ae849448011ca08c3785f1de1a54c8a96d6c23f787f9d962b624edd4151d",
+    "31611159e7e6ff7843ea4627745e89225fc866621cfcfdbd40871af4413747cc",
+    "6ef7c9b15ecdd69083724b84cfdc2100351963488b51b4ea2fcbddf493fbec94"
+);
 
 async function sha256(str) {
     const encoder = new TextEncoder('utf-8');
@@ -16,119 +26,27 @@ function kanaToHira(str) {
     });
 }
 
+function turnCard(question) {
+    var thisQuestion = document.getElementById('set'+question.toString());
+    thisQuestion.getElementsByClassName('question')[0].className = "surface";
+}
+
 async function onClick(question) {
-    if (question == 1) {
-        if(await sha256(kanaToHira(document.getElementById('a1').value)) == "c884a2af4f0ab6fe08b2d7158ec3359fd880c779f238728914dd2a72823a65b2"){
-            document.getElementById('r1').textContent = "正解";
-            var nextQuestion = document.getElementById('set2');
-            if(nextQuestion.style.opacity == 0){
-                nextQuestion.style.visibility = 'visible';
-                nextQuestion.scrollIntoView({behavior: "smooth"});
-                nextQuestion.animate([{opacity: '0'}, {opacity: '1'}], 1000);
-                nextQuestion.style.opacity = 1;
-            }
+    var thisQuestion = document.getElementById('set'+question.toString());
+    var nextQuestion = document.getElementById('set'+(question+1).toString());
+    if(await sha256(kanaToHira(thisQuestion.getElementsByClassName('answer')[0].value.toLowerCase())) == ANSWER_HASH[question-1]){
+        if(question == 8){
+            window.location.href = document.getElementById('a8').value.toLowerCase()+'.html';
         }
-        else{
-            document.getElementById('r1').textContent = "不正解";
-        }
-    }
-    if (question == 2) {
-        if(await sha256(document.getElementById('a2').value.toLowerCase()) == "e9de08263f4ad49131d3dd1dc28694d26baa1e9c3033f39a4885d7690b06d5b3"){
-            document.getElementById('r2').textContent = "正解";
-            var nextQuestion = document.getElementById('set3');
-            if(nextQuestion.style.opacity == 0){
-                nextQuestion.style.visibility = 'visible';
-                nextQuestion.scrollIntoView({behavior: "smooth"});
-                nextQuestion.animate([{opacity: '0'}, {opacity: '1'}], 1000);
-                nextQuestion.style.opacity = 1;
-            }
-        }
-        else{
-            document.getElementById('r2').textContent = "不正解";
+        thisQuestion.getElementsByClassName('response')[0].textContent = "正解";
+        if(nextQuestion.style.visibility != 'visible'){
+            nextQuestion.style.visibility = 'visible';
+            nextQuestion.scrollIntoView({behavior: "smooth"});
+            nextQuestion.getElementsByClassName('question')[0].src = 'img/card_back.png';
+            nextQuestion.animate([{opacity: '0'}, {opacity: '1'}], {duration: 1000, fill: "both"});
         }
     }
-    if (question == 3) {
-        if(await sha256(kanaToHira(document.getElementById('a3').value)) == "4a41357c7d510b4fc7cee057b0e9452a3548217c91264dca9200e6de5e77721f"){
-            document.getElementById('r3').textContent = "正解";
-            var nextQuestion = document.getElementById('set4');
-            if(nextQuestion.style.opacity == 0){
-                nextQuestion.style.visibility = 'visible';
-                nextQuestion.scrollIntoView({behavior: "smooth"});
-                nextQuestion.animate([{opacity: '0'}, {opacity: '1'}], 1000);
-                nextQuestion.style.opacity = 1;
-            }
-        }
-        else{
-            document.getElementById('r3').textContent = "不正解";
-        }
-    }
-    if (question == 4) {
-        if(await sha256(document.getElementById('a4').value.toLowerCase()) == "e81dfe69841ad2f7b5790b63e998f0febaf3b29acd732881975130761b98e2c7"){
-            document.getElementById('r4').textContent = "正解";
-            var nextQuestion = document.getElementById('set5');
-            if(nextQuestion.style.opacity == 0){
-                nextQuestion.style.visibility = 'visible';
-                nextQuestion.scrollIntoView({behavior: "smooth"});
-                nextQuestion.animate([{opacity: '0'}, {opacity: '1'}], 1000);
-                nextQuestion.style.opacity = 1;
-            }
-        }
-        else{
-            document.getElementById('r4').textContent = "不正解";
-        }
-    }
-    if (question == 5) {
-        if(await sha256(document.getElementById('a5').value.toLowerCase()) == "599f8341fe3e0fd23de7d89500b8224965ab536307049a175bcd30331d0918c5"){
-            document.getElementById('r5').textContent = "正解";
-            var nextQuestion = document.getElementById('set6');
-            if(nextQuestion.style.opacity == 0){
-                nextQuestion.style.visibility = 'visible';
-                nextQuestion.scrollIntoView({behavior: "smooth"});
-                nextQuestion.animate([{opacity: '0'}, {opacity: '1'}], 1000);
-                nextQuestion.style.opacity = 1;
-            }
-        }
-        else{
-            document.getElementById('r5').textContent = "不正解";
-        }
-    }
-    if (question == 6) {
-        if(await sha256(document.getElementById('a6').value.toLowerCase()) == "5944ae849448011ca08c3785f1de1a54c8a96d6c23f787f9d962b624edd4151d"){
-            document.getElementById('r6').textContent = "正解";
-            var nextQuestion = document.getElementById('set7');
-            if(nextQuestion.style.opacity == 0){
-                nextQuestion.style.visibility = 'visible';
-                nextQuestion.scrollIntoView({behavior: "smooth"});
-                nextQuestion.animate([{opacity: '0'}, {opacity: '1'}], 1000);
-                nextQuestion.style.opacity = 1;
-            }
-        }
-        else{
-            document.getElementById('r6').textContent = "不正解";
-        }
-    }
-    if (question == 7) {
-        if(await sha256(document.getElementById('a7').value.toLowerCase()) == "31611159e7e6ff7843ea4627745e89225fc866621cfcfdbd40871af4413747cc"){
-            document.getElementById('r7').textContent = "正解";
-            var nextQuestion = document.getElementById('set8');
-            if(nextQuestion.style.opacity == 0){
-                nextQuestion.style.visibility = 'visible';
-                nextQuestion.scrollIntoView({behavior: "smooth"});
-                nextQuestion.animate([{opacity: '0'}, {opacity: '1'}], 1000);
-                nextQuestion.style.opacity = 1;
-            }
-        }
-        else{
-            document.getElementById('r7').textContent = "不正解";
-        }
-    }
-    if (question == 8) {
-        if(await sha256(document.getElementById('a8').value.toLowerCase()) == "8f90d85ec13a0b1b12d81203f2f3262ca156028a94aeaf91ae52fcd5a9de4170"){
-            document.getElementById('r8').textContent = "正解";
-            window.location.href = 'https://godofmegane.github.io/HighAndLow/'+document.getElementById('a8').value.toLowerCase()+'.html';
-        }
-        else{
-            document.getElementById('r6').textContent = "不正解";
-        }
+    else{
+        thisQuestion.getElementsByClassName('response')[0].textContent = "不正解";
     }
 }
